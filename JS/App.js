b4184ccf17df06e07b26ca1b2235315260139f54
unknown <louismerit3@gmail.com>
2024-08-20 16:58:21 +0100

    Designing A car racer

--- JS/App.js
+++ JS/App.js
@@ -1,5 +1,22 @@
 function App (){
 
+  React.useEffect(() => {
+    $(function(){
+      $('go').click(function(){
+        // get the width of the cars
+        var carWidth = $('.car1').width();
+
+        //get the width of the racetrack
+        var raceTrackWidth = $(window).width() - carWidth;
+
+        // generate a random # between 1 and 5000
+
+        var raceTime1 = Math.floor((Math.random() * 4000) + 1)
+        var raceTime2 = Math.floor((Math.random() * 4000) + 1)
+      })
+    })
+  }, []);
+
   return(
     <>
     <h1 className="h1">jQuery Car Racer 3.1</h1>
@@ -10,8 +27,8 @@ function App (){
     </div>
     
     <div id="racetrack">
-      <img src="/Asset/car22.png" alt="Car 1" id="car1" className="car" width="200px" height="100px"/>
-      <img src="/Asset/image4.png" alt="car 2" id="car2" className="car" width="150px" height="100px"/>
+      <img src="/Asset/car1.png" alt="Car 1" id="car1" className="car" width="200px" height="100px"/>
+      <img src="/Asset/car2.png" alt="car 2" id="car2" className="car" width="150px" height="100px"/>
     </div>
     <div id="raceInfoContainer" class="clearfix">
       <h2>Results</h2>
