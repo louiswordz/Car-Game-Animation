a114ebdb98b31a6ad26547cfbdf4685889915e41
unknown <louismerit3@gmail.com>
2024-08-20 18:24:55 +0100

    Designed a car with React & JQuery

--- JS/App.js
+++ JS/App.js
@@ -1,20 +1,70 @@
 function App (){
-
+  
   React.useEffect(() => {
     $(function(){
-      $('go').click(function(){
+      $('#go').click(function(){
+        // Build a function that ckecks to see if a car has won
+      function checkIfComplete(){
+        if(isComplete == false){
+          isComplete = true;
+        }else{
+          place = "second";
+        }
+      }
+        
+
         // get the width of the cars
-        var carWidth = $('.car1').width();
+        var carWidth = $('#car1').width();
 
         //get the width of the racetrack
         var raceTrackWidth = $(window).width() - carWidth;
 
-        // generate a random # between 1 and 5000
+        // generate a random # between 1 and 4000
 
-        var raceTime1 = Math.floor((Math.random() * 4000) + 1)
-        var raceTime2 = Math.floor((Math.random() * 4000) + 1)
+        var raceTime1 = Math.floor((Math.random() * 4000) + 1);
+        var raceTime2 = Math.floor((Math.random() * 4000) + 1);
+
+        // set a flag variable to False
+        var isComplete = false;
+
+        // Set a flag variable to first default
+        var place = 'first';
+
+        // Animate car 1
+        $('#car1').animate({
+          //move the car width of the race track
+          left : raceTrackWidth
+        }, raceTime1, function(){
+          // Animation complete
+        })
+
+        // Run a function
+        checkIfComplete()
+
+        // give some text feedback in the race info box
+        $('#raceInfo1 span').text(`Finished in ${place} place and clocked in at ${raceTime1} milliseconds`)
+
+        // Animate car 2
+        $('#car2').animate({
+          //move the car width of the race track
+          left : raceTrackWidth +1
+        }, raceTime2, function(){
+          // Animation complete
+        })
+
+        // Run a function
+        checkIfComplete()
+
+        // give some text feedback in the race info box
+        $('#raceInfo2 span').text(`Finished in ${place} place and clocked in at ${raceTime2} milliseconds`)
+
+      });
+      $('#reset').on('click', function(){
+        $('.car').css('left','0')
+        $('.raceInfo span').text("")
       })
     })
+    
   }, []);
 
   return(
@@ -22,7 +72,7 @@ function App (){
     <h1 className="h1">jQuery Car Racer 3.1</h1>
     <div className="container button"> 
     <input type="button" value="Race1" id="go" className="btn btn-success" />
-    <input type ="button"  value="Reset" id="reset" className="btn btn-info" />
+    <input type ="button"  value="Reset" id="reset" className="btn btn-warning" />
 
     </div>
     
