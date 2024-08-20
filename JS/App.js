a1a9d9b3ea3eeaea9627d7dfe5612fe9c1e0c514
unknown <louismerit3@gmail.com>
2024-08-20 16:30:23 +0100

    Designing A car racer

---
+++ JS/App.js
@@ -0,0 +1,24 @@
+function App (){
+
+  return(
+    <>
+    <h1 className="h1">jQuery Car Racer 3.1</h1>
+    <div className="container button"> 
+    <input type="button" value="Race1" id="go" className="btn btn-success" />
+    <input type ="button"  value="Reset" id="reset" className="btn btn-info" />
+
+    </div>
+    
+    <div id="racetrack">
+      <img src="/Asset/car22.png" alt="Car 1" id="car1" className="car" width="200px" height="100px"/>
+      <img src="/Asset/image4.png" alt="car 2" id="car2" className="car" width="150px" height="100px"/>
+    </div>
+    <div id="raceInfoContainer" class="clearfix">
+      <h2>Results</h2>
+      <div id="raceInfo1" className="raceInfo">Car 1 <span></span></div>
+      <div id="raceInfo2" className="raceInfo">Car 2 <span></span></div>
+   </div>
+
+    </>
+    )
+}
